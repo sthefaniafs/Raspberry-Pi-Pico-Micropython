0cdae6a9717f3ac114537d8b510f5678efe3f098
sthefaniafs <sthefaniafernandes03@gmail.com>
2023-11-23 19:33:07 -0300

    update sensor bmp180

--- Projeto - Sensor BMP180/sensor_bmp180.py
+++ Projeto - Sensor BMP180/sensor_bmp180.py
@@ -1,13 +1,18 @@
 from machine import I2C, Pin
 from bmp180 import BMP180
+import utime
 
-i2c =  I2C(scl=Pin(15), sda=Pin(14), freq=100000)   # on esp8266
+i2c =I2C(id=0, scl=Pin(1), sda=Pin(0), freq=100000)
 bmp180 = BMP180(i2c)
 bmp180.oversample_sett = 2
 bmp180.baseline = 101325
 
-while true:
+while True:
     temp = bmp180.temperature
     p = bmp180.pressure
     altitude = bmp180.altitude
-    print(temp, p, altitude)
+    print("Temperatura:", temp, "ºC")
+    print ("Pressao:", p,"Pa" )
+    print ("Altitude:", altitude, "m")
+    utime.sleep(5)
+
